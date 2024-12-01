705b993f0a0c6b396bca5d5b13bbbb033004ad48
anubhav126 <tinfinite1248@gmail.com>
2024-12-01 22:26:26 +0530

    updated some text

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -78,7 +78,7 @@ export const Chart = ({ used = 0 }: { used: number }) => {
                           y={(viewBox.cy || 0) + 24}
                           className="fill-white/70"
                         >
-                          Space used
+                          The Total Space used is
                         </tspan>
                       </text>
                     );
