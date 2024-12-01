e97ad656dad82c86404686a5835a7266b3270d7b
anubhav126 <tinfinite1248@gmail.com>
2024-12-02 00:11:16 +0530

    color mods

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,26 +10,34 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
-  			brand: {
-  				'100': '#EA6365',
-  				DEFAULT: '#FA7275'
-  			},
-  			red: '#FF7474',
-  			error: '#b80000',
-  			green: '#3DD9B3',
-  			blue: '#56B8FF',
-  			pink: '#EEA8FD',
-  			orange: '#F9AB72',
-  			light: {
-  				'100': '#333F4E',
-  				'200': '#A3B2C7',
-  				'300': '#F2F5F9',
-  				'400': '#F2F4F8'
-  			},
-  			dark: {
-  				'100': '#04050C',
-  				'200': '#131524'
-  			},
+			brand: {
+				'100': '#A3B09A',
+				DEFAULT: '#FF2929',
+				'200': '#8DA389',
+				'300': '#778E78',
+				'400': '#637665',
+				'500': '#506254',
+				'600': '#3C4D44',
+				'700': '#293934',
+				'800': '#162523',
+				'900': '#051412',
+			  },
+			  red: '#FF7474',
+			  error: '#b80000',
+			  green: '#3DD9B3',
+			  blue: '#56B8FF',
+			  pink: '#EEA8FD',
+			  orange: '#F9AB72',
+			  light: {
+				'100': '#333F4E',
+				'200': '#A3B2C7',
+				'300': '#F2F5F9',
+				'400': '#F2F4F8',
+			  },
+			  dark: {
+				'100': '#04050C',
+				'200': '#131524',
+			  },
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
